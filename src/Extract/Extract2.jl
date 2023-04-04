1e13da227eec9062d0d9f60a5d5208aef2a3610b
Jakob Hoffmann <jfb-hoffmann@hotmail.de>
2023-04-04 11:56:31 +0200

    remove duplicate code

--- src/Extract/Extract2.jl
+++ src/Extract/Extract2.jl
@@ -10,61 +10,6 @@ const CPCFILE = "data/augment/patentinfo.csv"
 const PDFDIR = "data/raw/pdf_filtered"
 const RESFILE = "data/processed/data"
 
-function parse_date(filename::AbstractString)
-  m = match(r"[0-9]{1,2}(\.|,|\s)[0-9]{1,2}(\.|,|\s)[0-9]{4,}", filename).match
-  m = replace(m, r",|\s" => ".")
-  Date(m, "dd.mm.yyyy")
-end
-
-function get_casenumber(s::AbstractString)
-  m = match(r"[0-9]{1,2}\s{0,3}[a-zA-Z]{1,2}\s{0,3}(\(pat\))?\s{0,3}[0-9]{1,3}\/[0-9]{1,2}(\s{0,3}\((EP|EU)\))?", s)
-  isnothing(m) && return ""
-  m.match
-end
-get_casenumber(s::AbstractVector) = get_casenumber(join(first(s, 5)))
-
-function get_senate(s::AbstractString)
-  m = match(r"[0-9]\.*\s*Senat", s)
-  isnothing(m) && return 0
-  parse(Int, match(r"[0-9]{1,2}", m.match).match)
-end
-get_senate(s::AbstractVector) = get_senate(join(first(s, 5)))
-
-function get_patent(s::AbstractString)
-  m = match(r"betreffend\s{0,3}das\s{0,3}(europäische|deutsche)?\s{0,3}Patent\s{0,3}(DE|EP)?([0-9]|\s){6,20}"i, s)
-  isnothing(m) && return missing
-  id = match(r"((DE|EP)?[0-9|\s]{6,20})", m.match).match
-  id = replace(id, r"\s{1,10}" => "")
-  if startswith(id, r"EP|DE")
-    return id
-  elseif m.captures[1] == "europäische"
-    return "EP" * id
-  elseif m.captures[1] == "deutsche"
-    return "DE" * id
-  else
-    return id
-  end
-end
-get_patent(s::AbstractVector) = get_patent(join(first(s, 5)))
-
-function get_outcome(s::AbstractString)
-  _cleanwhitespace(s) = replace(s, r"\s{1,}" => " ")
-
-  RESULTMATCHES = dictionary([
-    r"teilweise für nichtig erklärt"i => "partially annulled",
-    r"im Umfang (der|seiner) (patent)?ansprüche(.*)für nichtig erklärt"i => "partially annulled",
-    r"für nichtig erklärt, soweit" => "partially annulled",
-    r"für nichtig erklärt"i => "annulled",
-    r"die Klagen? (wird|werden) abgewiesen"i => "claim dismissed",
-  ])
-  m = map(keys(RESULTMATCHES)) do m
-    !isnothing(match(m, _cleanwhitespace(s)))
-  end
-  all(m .== false) && return missing
-  RESULTMATCHES[findfirst(m)] # TODO: Should not just pick first result
-end
-get_outcome(s::AbstractVector) = get_outcome(join(first(s, 5)))
-
 function extractinfo(docs)
   DataFrame(
     file=docs.file,
@@ -87,30 +32,6 @@ function unflatten_results(df)
   res.judges = splitm(res.judges)
   res.cpc = splitm(res.cpc)
   res
-end
-
-function write_csv(out, result::DataFrame)
-  CSV.write(out * ".csv", result)
-end
-function write_json(out, result::DataFrame)
-  res = unflatten_results(result)
-  open(out * ".jsonl", "w") do io
-    for row in Tables.rowtable(res)
-      JSON3.write(io, row)
-      print(io, "\n")
-    end
-  end
-end
-
-function csv2json(csv, json)
-  csv = CSV.read(csv, DataFrame)
-  res = unflatten_results(csv)
-  open(json, "w") do io
-    for row in Tables.rowtable(res)
-      JSON3.write(io, row)
-      print(io, "\n")
-    end
-  end
 end
 
 function main()
